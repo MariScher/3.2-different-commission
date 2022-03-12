226408a36fd63bd97979395750f9ca7b1c67b46e
MariScher <marina_vetr@mail.ru>
2022-03-12 19:25:24 +0300

    "Finally"

--- src/main/kotlin/3.2-different-commission.kt
+++ src/main/kotlin/3.2-different-commission.kt
@@ -1,33 +1,31 @@
+enum class CardType {
+    Maestro, MC, Visa, Mir, VkPay
+}
 fun main() {
-    val cardType = "VkPay"
     val previousAmount = 0
     val amount = 15_000_00
 
     val limit = amount + previousAmount
-    val result = totalFee(cardType, amount, limit,
-        calculateFee(cardType, amount, limit)
-    )
+    totalFee(cardType = CardType.VkPay, limit, amount)
 }
 
-fun totalFee(cardType: String, amount: Int, limit: Int, calculateFee: Int) {
-
-    if (cardType == "Maestro_MC" || cardType == "Visa_Mir" && limit < 600_000_00) {
-        println("$calculateFee")
-    } else if (cardType == "VkPay" && limit < 40_000_00) {
+fun totalFee(cardType: CardType, limit: Int, amount: Int) =
+    if (cardType == CardType.Maestro || cardType == CardType.MC ||
+        cardType == CardType.Visa || cardType == CardType.Mir && limit < 600_000_00) {
+        val totalFee = calculateFee(cardType, amount, limit)
+        println("$totalFee")
+    } else if (cardType == CardType.VkPay && limit < 40_000_00) {
         println(0)
     } else {
         println("Платеж невозможен, превышен месячный лимит")
     }
-}
 
-fun calculateFee(cardType: String, amount: Int, limit: Int): Int {
-    return when (cardType) {
-        "Maestro_MC" -> {
-            if (limit > 75_000_00) (amount * 0.006 + 20).toInt() else 0
-        }
-        "Visa_Mir" -> {
-            if (limit > 35_00) (amount * 0.0075).toInt() else 35_00
-        }
-        else -> 0
+fun calculateFee(cardType: CardType, amount: Int, limit: Int) = when (cardType) {
+    CardType.Maestro, CardType.MC -> {
+        if (limit > 75_000_00) (amount * 0.006 + 20).toInt() else 0
     }
+    CardType.Visa, CardType.Mir -> {
+        if (limit > 35_00) (amount * 0.0075).toInt() else 35_00
+    }
+    CardType.VkPay -> 0
 }
